c0a711e7a486dc3435dfe5fdae91d127293b3432
Rodri <rodrigosib2017@gmail.com>
2021-04-24 10:53:30 -0300

    .not added to test

--- exercises.test.js
+++ exercises.test.js
@@ -6,14 +6,17 @@ test("return a string", () => {
 
 test("adds 1 + 2 to equal 3", () => {
   expect(exercises.suma(1, 2)).toBe(3);
+  expect(exercises.suma(1, 2)).not.toBe("3");
 });
 
 test("rest 4 - 2 to equal 2", () => {
   expect(exercises.resta(4, 2)).toBe(2);
+  expect(exercises.resta(4, 2)).not.toBe("2");
 });
 
 test("multiply 2 x 4 to equal 8", () => {
   expect(exercises.multiplicacion(2, 4)).toBe(8);
+  expect(exercises.multiplicacion(2, 4)).not.toBe("8");
 });
 
 test("can not divide by 0", () => {
@@ -22,6 +25,7 @@ test("can not divide by 0", () => {
 
 test("divide 8 by 2 to equal 4", () => {
   expect(exercises.division(8, 2)).toBe(4);
+  expect(exercises.division(8, 2)).not.toBe("4");
 });
 
 test("if multiplicador is undefined then multiplicador is equal to 10", () => {
@@ -34,10 +38,12 @@ test("multiplicando is 2 and multiplicador 4", () => {
 
 test("2 cubed to equal 8", () => {
   expect(exercises.potencia(2, 3)).toBe(8);
+  expect(exercises.potencia(2, 3)).not.toBe("8");
 });
 
 test("2^2 + 4^4 to be 20", () => {
   expect(exercises.anidada(2, 4)).toBe(20);
+  expect(exercises.anidada(2, 4)).not.toBe("20");
 });
 
 test("2 to have three 0 to the left", () => {
