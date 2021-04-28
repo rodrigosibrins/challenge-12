58a2ef6611b463c18e859330fdd7cf976b8f5e1e
Rodri <rodrigosib2017@gmail.com>
2021-04-28 13:41:05 -0300

    more jest functions added

--- exercises.test.js
+++ exercises.test.js
@@ -7,14 +7,23 @@ describe("Function return a string", () => {
   test("Return 'text'", () => {
     expect(exercises.texto("text")).toBe("text");
   });
+  test("Return a word from string", () => {
+    expect(exercises.texto("This is a long text to test the function")).toContain("long");
+  });
 });
 
 describe("Function display sums of two values", () => {
   test("Adds 1 + 2 to equal 3", () => {
     expect(exercises.suma(1, 2)).toBe(3);
   });
+  test("Adds - 1 - 2 to equal -3", () => {
+    expect(exercises.suma(-1, -2)).toBe(-3);
+  });
   test("Function doesn't return a string value", () => {
     expect(exercises.suma(1, 2)).not.toBe("3");
+  });
+  test("Function doesn't return a string value", () => {
+    expect(exercises.suma(5670, 3405)).toBeGreaterThan(9000);
   });
 });
 
@@ -22,12 +31,21 @@ describe("Rest function", ()=> {
   test("Rest 4 - 2 to equal 2", () => {
     expect(exercises.resta(4, 2)).toBe(2);
   });
+  test("Adds - 4 - 2 to equal -6", () => {
+    expect(exercises.suma(-4, -2)).toBe(-6);
+  });
   test("The returned value is not a string", () => {
     expect(exercises.resta(4, 2)).not.toBe("2");
+  });
+  test("The returned value is not a string", () => {
+    expect(exercises.resta(-1, -1)).toBeLessThanOrEqual(0);
   });
 });
 
 describe("Multiply function", () => {
+  test("Multiply 2 x 4 to equal 8", () => {
+    expect(exercises.multiplicacion(2, 4)).toBeTruthy();
+  });
   test("Multiply 2 x 4 to equal 8", () => {
     expect(exercises.multiplicacion(2, 4)).toBe(8);
   });
@@ -46,6 +64,9 @@ describe("Division function", () => {
   test("The returned value is not a string", () => {
     expect(exercises.division(8, 2)).not.toBe("4");
   });
+  test('Divide 7 by 6 close to 1', () => {
+    expect(exercises.division(7, 6)).toBeCloseTo(1.16, 0.5);
+  });
 });
 
 describe("Multiplication table function", () => {
@@ -61,6 +82,9 @@ describe("Multiplication table function", () => {
 });
 
 describe("Exponent function", () => {
+  test("0 cubed to be false", () => {
+    expect(exercises.potencia(0, 3)).toBeFalsy();
+  });
   test("2 cubed to equal 8", () => {
     expect(exercises.potencia(2, 3)).toBe(8);
   });
@@ -75,6 +99,9 @@ describe("Nested function", () => {
   });
   test("The returned value is not a string", () => {
     expect(exercises.anidada(2, 4)).not.toBe("20");
+  });
+  test("The returned value is not a string", () => {
+    expect(exercises.anidada(2, 4)).not.toBeNull();
   });
 });
 
